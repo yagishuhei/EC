c806ffec1bda6a61027cf0f9a44d8b3171b59dac
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2022-05-27 17:17:37 +0000

    [Update]routing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
+  config.hosts << "c6e233fdfd12423583a81485ea5a7ee9.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  get 'admin/genres'
-  get 'admin/index'
-  get 'admin/edit'
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  get 'addresses/index'
-  get 'addresses/edit'
-  namespace :public do
-    get 'orders/new'
-    get 'orders/comfirm'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  #顧客用
+
+  #会員用
   #URL/customer/sign_in ...
   #skipで余分なルーティングを削除＋app/views/public/shared/_links.html.erbの余分な記述を削除
   devise_for :customers, skip:[:passwords], controllers:{
@@ -61,6 +15,44 @@ Rails.application.routes.draw do
   devise_for :admin, skip:[:registrations, :passwords], controllers:{
     sessions: "admin/sessions"
   }
+
+  #会員用
+  #URLはそのまま、ファイル構成だけ指定のパス
+  scope module: :public do
+
+    get root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: 'about'
+
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all', to: 'cart_items#destroy_all'
+    resources :customers, only: [:show, :edit, :update]
+    #会員の退会確認画面
+    get 'customers/unsubscribe', to: 'customers#unsubscribe'
+    #会員の退会処理
+    patch 'customers/withdraw', to: 'customers#withdrow'
+    resources :items, only: [:index, :show]
+    resources :orders, only: [:new, :create, :index, :show]
+    #注文情報確認画面
+    post 'orders/comfirm', to: 'orders#comfirm'
+    #注文完了画面
+    get 'orders/complete', to: 'orders#complete'
+
+  end
+
+  #管理者用
+  #URLはadminをつけて、ファイル構成も指定
+  namespace :admin do
+
+    get root to: 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+
+  end
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
